732f8fdf4917d1425d590e1e5b46eb947340478a
unknown <SENAI@C303-LAB05-30.senaiescola.fsa>
2024-11-09 13:57:58 -0300

    Ajustando script do database

--- Banco de Dados/script_criacao_banco.sql
+++ Banco de Dados/script_criacao_banco.sql
@@ -26,3 +26,32 @@ CREATE TABLE ocorrencias_trote (
 );
 
 SHOW TABLES;
+
+DELIMITER $$
+
+CREATE OR REPLACE FUNCTION buscar_telefone_id(telefone_numero VARCHAR(20))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE telefone_id INT;
+
+    -- Usando LIKE para buscar números de telefone que contenham o número informado
+    SELECT id INTO telefone_id
+    FROM telefones
+    WHERE numero LIKE CONCAT('%', telefone_numero, '%')  -- % garante que qualquer parte do número será encontrada
+    LIMIT 1;
+
+    RETURN telefone_id;
+END $$
+
+DELIMITER ;
+
+
+INSERT INTO telefones (numero) VALUES ('(75) 99199-2950');
+
+SELECT buscar_telefone_id('9919');
+
+SELECT buscar_telefone_id('(75) 99199-2950');
+
+DELETE FROM telefones WHERE id = 1;
+
