f8d712ee6f04bf8f889f4e9f6ab405732016424a
Thiago Trabuco <163602179+DevThiagoTrabuco@users.noreply.github.com>
2024-11-09 16:53:18 -0300

    update

--- TroteBuster/src/main/java/DAO/OcorrenciaDAO.java
+++ TroteBuster/src/main/java/DAO/OcorrenciaDAO.java
@@ -4,9 +4,7 @@ import DTO.OcorrenciaDTO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 
